b55bf6cb191bf2a764bdadc03364c3875734cf7f
RafaelCOliveira <rafacoliveira07@gmail.com>
2024-09-05 22:50:45 -0300

    ex3 update

--- JS/Lista01/Ex3.js
+++ JS/Lista01/Ex3.js
@@ -1,2 +1,11 @@
 let prompt = require('prompt-sync')()
 
+let nota = Number(prompt('Digite a sua nota: '))
+
+if (nota >= 0 && nota < 6){
+    console.log('Reprovado.')
+} else if (nota >= 6 && nota < 7) {
+    console.log('Recuperação')
+} else if (nota >= 7 && nota <= 10){
+    console.log('Aprovado')
+}
